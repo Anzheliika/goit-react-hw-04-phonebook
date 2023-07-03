5b3cc02b7d6a67247debde8385200656e7db7c4d
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-07-03 16:33:36 +0300

    add useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
 import { Phonebook } from './App.styled';
-import useLocalStorage from '../../hooks/useLocalStorage';
 import toast, { Toaster } from 'react-hot-toast';
+import { nanoid } from 'nanoid';
 
 function App() {
-  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
   const [filter, setFilter] = useState('');
 
-  const addContact = newContact => {
-    contacts.some(
-      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-    )
-      ? toast(`${newContact.name} is already in contacts!`)
-      : setContacts(prevContact => [...prevContact, newContact]);
+  useEffect(() => {
+    console.log('CONTACTS');
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const contact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      return toast(`${name} is already in contacts!`);
+    }
+    return setContacts([contact, ...contacts]);
   };
 
   const getVisibleContacts = () => {
